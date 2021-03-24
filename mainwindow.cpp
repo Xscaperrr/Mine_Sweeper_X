91dd7005a23dad51a13b174fdce241f96492b474
Xscaper- <62665519+Xscaperrr@users.noreply.github.com>
2021-03-24 20:25:22 +0800

    3.24

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,27 @@
 #include "mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
+    : QMainWindow(parent),
+      scene(new QGraphicsScene(this)),
+      view(new GraphicsView(scene,this)),
+      menubar(new QMenuBar(this))
+
 {
+    resize(1600,900);
+    setCentralWidget(view);
+    setMenuBar(menubar);
 }
 
 MainWindow::~MainWindow()
 {
 }
+
+GraphicsView::GraphicsView(QGraphicsScene *scene,QWidget *parent)
+{
+    setScene(scene);setParent(parent);
+}
+GraphicsView::~GraphicsView()
+{    
+}
+
 
--- mainwindow.h
+++ mainwindow.h
@@ -2,13 +2,33 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QGraphicsView>
+#include <QGraphicsScene>
+#include <QGraphicsItem>
+#include <QMenuBar>
+#include <QAction>
+
+class GraphicsView : public QGraphicsView
+{
+Q_OBJECT
+
+public:
+GraphicsView(QGraphicsScene *scene=nullptr,QWidget *parent = nullptr);
+~GraphicsView();
+};
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
+    QGraphicsScene *scene;
+    GraphicsView *view;
+    QMenuBar *menubar;
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 };
+
+
+
 #endif // MAINWINDOW_H
